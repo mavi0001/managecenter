eaad8d5b8b6743773a804ea8ebd596cd095928d2
selmabou <bouabidisalma07@gmail.com>
2025-03-05 22:21:41 +0000

    navbar

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,17 +1,13 @@
 <x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Home') }}
-        </h2>
-    </x-slot>
-
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900">
-                    {{ __("You're logged in!") }}
+    <div class="py-12 bg-white min-h-screen flex items-center justify-center">
+        <div class="max-w-4xl w-full mx-auto px-6">
+            <div>
+                <div class="p-10 text-[#3A406D] text-center">
+                    <h3 class="text-4xl font-bold">{{ __("Vous êtes connecté!") }}</h3>
+                    <p class="mt-4 text-lg text-[#3A406D]">Bienvenue sur la plateforme de gestion du Centre d'Accueil – Espace Administrateur</p>
                 </div>
             </div>
         </div>
     </div>
+
 </x-app-layout>
